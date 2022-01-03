8e545a8e21f1d56a8eb00795bb54267694a8e0b9
KrStasya <anastasiya.krautsova@gmail.com>
2022-01-03 16:07:12 +0300

    Refactor

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -8,7 +8,7 @@ function Header (){
             <div className={s.navigate}>
                 <NavLink to="registration" className={s.Linktopage}>registration</NavLink>
                 <NavLink to={"login"} className={s.Linktopage}>login</NavLink>
-                <NavLink to={"profile"} className={s.Linktopage}>registration</NavLink>
+                <NavLink to={"profile"} className={s.Linktopage}>profile</NavLink>
                 <NavLink to={"/404"} className={s.Linktopage}>404</NavLink>
                 <NavLink to={"password_recovery"} className={s.Linktopage}>password recovery</NavLink>
                 <NavLink to={"new_password"} className={s.Linktopage}>new password</NavLink>
--- src/registration/Registration.tsx
+++ src/registration/Registration.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Registration=()=>{
     return (
         <div>
-
+            <h4>Hello</h4>
         </div>
     )
 }
